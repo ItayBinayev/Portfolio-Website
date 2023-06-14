e90d524ca3920116ba526b22c7ba2f94bc7bc6ac
Itay <itaybinayev@gmail.com>
2023-06-14 13:29:32 +0300

    Adding title

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -33,6 +33,7 @@ export default class MyDocument extends Document {
         <Head >
         <link rel="icon" href="/BracketsFavi.png" />
           <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap" rel="stylesheet"/>
+          <title>Itay Binayev</title>
         </Head>
         <body>
           <Main />
