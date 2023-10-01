2309b9abd44aa23aeaeab1d7751b9be5df0bcd66
Itay <itaybinayev@gmail.com>
2023-10-01 10:42:34 +0300

    changed order of projects

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,21 +1,12 @@
 export const projects = [
   {
-    title: 'Shorten URL Project',
-    description: "The project is developed using a combination of JavaScript, ASP.NET, Bootstrap, and MSSQL. It aims to provide a URL shortening service, allowing users to input a long URL and obtain a shorter, more manageable URL that redirects to the original destination.",
-      image: '/images/ShortenURL.png',
-      tags: ['Javascript', 'ASP.NET', 'Bootstrap', 'MSSQL'],
-    source: 'https://github.com/ItayBinayev/Shorten-URL-Project',
-    visit: '',
+    title: 'Streaming System (Like Netflix)',
+    description:"A highly professional project utilizing Node.js, Express, MongoDB, and React.js. The project emulates a Netflix-like platform and icorporates a robust authentication system using JWT.  ",
+    image: '/images/NetflixProject.png',
+    tags: ['MongoDB', 'Express','React','Node.js'],
+    source: 'https://github.com/ItayBinayev/netflix-frontend',
+    visit: 'https://netflix-frontend-phi.vercel.app/',
     id: 0,
-  },
-  {
-    title: 'Terminal Simulator',
-    description:"A project using React, ASP.NET, MSSQL, and NLog. The goal of this project is to create flights at regular intervals and update their positions. In this system, there can only be a single flight in places 4, 6, and 7. Whenever a flight leaves one of these places, an event is triggered, and a new flight takes its place. ",
-    image: '/images/TerminalSimulator.png',
-    tags: ['React', 'ASP.NET','MSSQL','NLog'],
-    source: 'https://github.com/ItayBinayev/TerminalSimulator',
-    visit: '',
-    id: 1,
   },
   {
     title: 'E-Shop (Like Amazon)',
@@ -24,18 +15,26 @@ export const projects = [
     tags: ['MongoDB', 'Express','React','Node.js'],
     source: 'https://github.com/ItayBinayev/amazon-frontend',
     visit: 'https://amazon-frontend-ten.vercel.app/',
+    id: 1,
+  },
+  {
+    title: 'Terminal Simulator',
+    description:"A project using React, ASP.NET, MSSQL, and NLog. The goal of this project is to create flights at regular intervals and update their positions. In this system, there can only be a single flight in places 4, 6, and 7. Whenever a flight leaves one of these places, an event is triggered, and a new flight takes its place. ",
+    image: '/images/TerminalSimulator.png',
+    tags: ['React', 'ASP.NET','MSSQL','NLog'],
+    source: 'https://github.com/ItayBinayev/TerminalSimulator',
+    visit: '',
     id: 2,
   },
   {
-    title: 'Streaming System (Like Netflix)',
-    description:"A highly professional project utilizing Node.js, Express, MongoDB, and React.js. The project emulates a Netflix-like platform and icorporates a robust authentication system using JWT.  ",
-    image: '/images/NetflixProject.png',
-    tags: ['MongoDB', 'Express','React','Node.js'],
-    source: 'https://github.com/ItayBinayev/netflix-frontend',
-    visit: 'https://netflix-frontend-phi.vercel.app/',
+    title: 'Shorten URL Project',
+    description: "The project is developed using a combination of JavaScript, ASP.NET, Bootstrap, and MSSQL. It aims to provide a URL shortening service, allowing users to input a long URL and obtain a shorter, more manageable URL that redirects to the original destination.",
+      image: '/images/ShortenURL.png',
+      tags: ['Javascript', 'ASP.NET', 'Bootstrap', 'MSSQL'],
+    source: 'https://github.com/ItayBinayev/Shorten-URL-Project',
+    visit: '',
     id: 3,
   },
-  
 ];
 
 export const TimeLineData = [
